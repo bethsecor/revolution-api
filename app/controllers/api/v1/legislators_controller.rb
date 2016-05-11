3ed8cc0bb982c9968760cad7bb8212f30b9879f0
Beth Secor <elizabeth.a.secor@gmail.com>
2016-05-11 16:58:00 -0600

    Complete create legislator api endpoint, test passing

--- app/controllers/api/v1/legislators_controller.rb
+++ app/controllers/api/v1/legislators_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::LegislatorsController < Api::V1::ApiController
   def show
     respond_with Legislator.find(params[:id])
   end
+
+  def create
+    respond_with :api, :v1, Legislator.create(legislator_params)
+  end
+
+  private
+
+    def legislator_params
+      params.require(:legislator).permit(:name,
+                                         :state,
+                                         :district,
+                                         :political_party,
+                                         :term_starts_on,
+                                         :term_ends_on)
+    end
 end
