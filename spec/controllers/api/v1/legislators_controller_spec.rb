f655e6da50813b0857143e4152b5bed6cfe078ae
Beth Secor <elizabeth.a.secor@gmail.com>
2016-05-11 16:15:14 -0600

    Test for legislator api show endpoint

---
+++ spec/controllers/api/v1/legislators_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::LegislatorsController, type: :controller do
+  it "show should return a successful response and return a legislator" do
+    legislator = Legislator.create(name: "John Smith",
+                                   state: "CA",
+                                   district: 1,
+                                   political_party: "independent",
+                                   term_starts_on: DateTime.new(2016, 02, 01),
+                                   term_ends_on: DateTime.new(2018, 02, 01))
+
+    get :show, id: legislator.id, format: :json
+
+    expect(response.status).to eq 200
+
+    api_legislator = JSON.parse(response.body)
+
+    expect(api_legislator['name']).to eq legislator.name
+    expect(api_legislator['state']).to eq legislator.state
+    expect(api_legislator['district']).to eq legislator.district
+    expect(api_legislator['political_party']).to eq legislator.political_party
+    expect(api_legislator['term_starts_on']).to eq "2016-02-01"
+    expect(api_legislator['term_ends_on']).to eq "2018-02-01"
+    expect(api_legislator['created_at']).to eq nil
+    expect(api_legislator['updated_at']).to eq nil
+  end
+end
